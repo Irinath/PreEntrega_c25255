ed59cdb44632bcc90ab0b8db1d143b279f40ad06
Valeria Thomas <122585324+Irinath@users.noreply.github.com>
2025-10-08 01:39:28 -0300

    preentrega  BEJava

---
+++ src/main/java/Articulo.java
@@ -0,0 +1,50 @@
+public class Articulo {
+
+    String nombre;
+    int idArticulo;
+    float precio;
+    int stock;
+
+
+    //Constructores para la clase Artículo
+    public Articulo(int idArticulo, String nombre, float precio, int stock){
+        this.idArticulo;
+        this.nombre;
+        this.stock;
+        this.precio;
+
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getIdArticulo() {
+        return idArticulo;
+    }
+
+    public void setIdArticulo(int idArticulo) {
+        this.idArticulo = idArticulo;
+    }
+
+    public float getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(float precio) {
+        this.precio = precio;
+
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+}

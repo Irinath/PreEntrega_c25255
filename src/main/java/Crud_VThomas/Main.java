b86088de156ad2e00dd673e96e287531b691e661
Valeria Thomas <thomas.valeria@gmail.com>
2025-10-24 01:26:33 -0300

    Completando el Main del CRUD

--- src/main/java/Crud_VThomas/Main.java
+++ src/main/java/Crud_VThomas/Main.java
@@ -1,4 +1,19 @@
 package Crud_VThomas;
-public static void main(String[] args) {
 
+import java.util.ArrayList;
+public class Main {
+
+    public static void main(String[] args) {
+
+        final ArrayList<Producto> productos = new ArrayList<>();
+        final ArrayList<Categoria> categorias = new ArrayList<>();
+
+        //Instancio Categoría Agregando items al ArrayList
+        categorias.add(new Categoria("Componentes"));
+        categorias.add(new Categoria("Insumos"));
+        categorias.add(new Categoria("Computadoras"));
+        categorias.add(new Categoria("Periféricos"));
+        categorias.add(new Categoria("Portátiles"));
+        categorias.add(new Categoria("Impresoras"));
+    }
 }
